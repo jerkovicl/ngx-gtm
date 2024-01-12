a5f4b3d3dc5468d44e22ff82d42cc112395c85d9
Luka Jerkovic <luka.jerkovic@serengetitech.com>
2024-01-12 15:34:44 +0100

    feat(ngx-gtm,demo): added example of using lib in demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { DataLayerService } from '@jerkovicl/ngx-gtm';
+import { filter } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -8,6 +11,31 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit {
+  public title = 'demo';
+  private readonly destroyRef: DestroyRef = inject(DestroyRef);
+  private readonly router: Router = inject(Router);
+  private readonly dataLayerService: DataLayerService = inject(DataLayerService);
+  ngOnInit(): void {
+    this.triggerRouterEvents();
+  }
+
+  private triggerRouterEvents() {
+    this.router.events
+      .pipe(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        filter((event: any) => event instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef),
+      )
+      .subscribe((event: NavigationEnd) => {
+        /** START : Code to track page view using GTM */
+        const gtmTag = {
+          event: 'page',
+          pageName: event.urlAfterRedirects,
+          data: {},
+        };
+        this.dataLayerService.logPageView(gtmTag);
+        /** END : Code to track page view using GTM */
+      });
+  }
 }
--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,8 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+  withRouterConfig,
+} from '@angular/router';
+import { provideGtm } from '@jerkovicl/ngx-gtm';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideRouter(appRoutes)],
+  providers: [
+    provideClientHydration(),
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
+      withEnabledBlockingInitialNavigation(),
+      withComponentInputBinding(),
+      withRouterConfig({ paramsInheritanceStrategy: 'always' }),
+    ),
+    provideAnimationsAsync(),
+    provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
+    provideHttpClient(),
+    provideGtm({
+      enabled: true,
+      id: 'xxxx',
+    }),
+  ],
 };
