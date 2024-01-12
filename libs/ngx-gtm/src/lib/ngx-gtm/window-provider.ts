874269fbe961ee8af4f3ea53e525aad7b76e9d72
Luka Jerkovic <luka.jerkovic@serengetitech.com>
2024-01-12 16:48:44 +0100

    fix(ngx-gtm): improve WINDOW token for SSR

--- libs/ngx-gtm/src/lib/ngx-gtm/window-provider.ts
+++ libs/ngx-gtm/src/lib/ngx-gtm/window-provider.ts
@@ -1,6 +1,15 @@
-import { InjectionToken } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { inject, InjectionToken } from '@angular/core';
 
 export const WINDOW = new InjectionToken<Window>('Global window object', {
   providedIn: 'root',
-  factory: () => window,
+  factory: () => {
+    const { defaultView } = inject(DOCUMENT);
+
+    if (!defaultView) {
+      throw new Error('Window is not available');
+    }
+
+    return defaultView;
+  },
 });
