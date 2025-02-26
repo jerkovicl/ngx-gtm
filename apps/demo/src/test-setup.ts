e47798e283dcf6fc2b4fdb95e0350b640985463b
Luka Jerkovic <luka.jerkovic@serengetitech.com>
2025-02-26 15:53:57 +0100

    fix(ngx-gtm): resolve test issues, migrate deprecated jest functions

--- apps/demo/src/test-setup.ts
+++ apps/demo/src/test-setup.ts
@@ -1,8 +1,15 @@
-// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
-globalThis.ngJest = {
-  testEnvironmentOptions: {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true,
-  },
-};
-import 'jest-preset-angular/setup-jest';
+import 'zone.js';
+
+import 'zone.js/testing';
+
+import { getTestBed } from '@angular/core/testing';
+// import '@angular/localize/init';
+
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv({ errorOnUnknownElements: true, errorOnUnknownProperties: true });
+getTestBed().resetTestEnvironment();
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: true },
+});
